d2815890c0b34cbc6d22d02c4b279df8e36b1656
Wetterpants <swetterstrom@gmail.com>
2026-02-02 15:20:18 -0600

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function toggleTips(id) {
-  const section = document.getElementById(id);
-
-  if (section.style.display === "none") {
-    section.style.display = "block";
-  } else {
-    section.style.display = "none";
-  }
-}
