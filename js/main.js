9f8f3aa8710ca43acfe1f49aa9dd7dd0b23826c1
Wetterpants <swetterstrom@gmail.com>
2026-02-02 15:21:24 -0600

    Add JavaScript toggle function

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggleTips(id) {
+  const section = document.getElementById(id);
+
+  if (section.style.display === "none") {
+    section.style.display = "block";
+  } else {
+    section.style.display = "none";
+  }
+}
